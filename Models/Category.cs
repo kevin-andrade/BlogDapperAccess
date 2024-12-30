2c5a348883bfb1a8791c7e738dad36b6c27791b0
keven-andrade <nivekandrade@live.com>
2024-12-30 15:44:51 -0300

    Add models Category, Post, User

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,10 +6,13 @@ namespace Blog.Models
 {
     [Table("[Category]")]
     public class Category : IRepository
-
     {
+        public Category()
+                => Posts = new List<Post>();
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
+        public List<Post> Posts { get; set; }
     }
 }
---
+++ Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
+{
+    [Table("[Post]")]
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
